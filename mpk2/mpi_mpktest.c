ecf0b65a5b651d9df6425639694d230399fa9a42
Emil VATAI <emil.vatai@gmail.com>
2019-06-15 09:50:18 +0900

    Add basic MPI code and fix makefile to use wrapper

--- mpk2/mpi_mpktest.c
+++ mpk2/mpi_mpktest.c
@@ -2,6 +2,9 @@
 
 int main(int argc, char* argv[]){
   MPI_Init(&argc, &argv);
+  int world_rank, world_size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Finalize();
   return 0;
 }
