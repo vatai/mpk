c309f9ba43b37c2ed4d64bb0d1ded40d638f60ff
Emil VATAI <emil.vatai@gmail.com>
2019-06-26 13:59:07 +0900

    Improve `test_builds.sh`

--- mpk2/test_builds.sh
+++ mpk2/test_builds.sh
@@ -7,20 +7,31 @@
 # time of writing mpi_mpktest, check if bot bot mpktest versions
 # perform give the same results.
 
+NAME=three
+SIZE=3
+NPART=4
+NLEVEL=10
+NPHASE=5
+
+MAKEFILE=makefile
+which mpiicc 2>/dev/null 1>/dev/null && MAKEFILE+=.intel || MAKEFILE+=.gcc
 # Force build all.
-make -B
+make -f $MAKEFILE -B
 
 # Delete input files.
-rm -rf five10*
+echo rm -rf ${NAME}${SIZE}_${NPART}_${NLEVEL}_${NPHASE}
+rm -rf ${NAME}${SIZE}_${NPART}_${NLEVEL}_${NPHASE}
 
 # Check `gen`, `driver` and `mpktest`
+# usage: ./gen type size ghead
+# usage: ./driver ghead npart nlevel nphase
 echo test_builds.sh: generating data
-./gen m5p 10 five10 && ./driver five10 4 20 5
+./gen m5p $SIZE $NAME$SIZE && ./driver $NAME$SIZE $NPART $NLEVEL $NPHASE
 
 echo test_builds.sh: testing OpenMP version
 # Check OpenMP version
-./mpktest five10_4_20_5
+./mpktest ${NAME}${SIZE}_${NPART}_${NLEVEL}_${NPHASE}
 
 echo test_builds.sh: testing MPI version
 # Check MPI version
-mpirun ./mpi_mpktest five10_4_20_5
+mpirun ./mpi_mpktest ${NAME}${SIZE}_${NPART}_${NLEVEL}_${NPHASE}
