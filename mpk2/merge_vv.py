a91d3c8e38d5feb9a1ff1ee6a2b155ec29428fe6
Emil VATAI <emil.vatai@gmail.com>
2019-07-05 17:06:42 +0900

    Add merge_vv.py python script

---
+++ mpk2/merge_vv.py
@@ -0,0 +1,55 @@
+import sys
+import glob
+
+N = 4
+fname_pattern = sys.argv[1]
+
+
+def open_files(fname_pattern):
+    """Open files. """
+    files = []
+    for fname in sorted(glob.glob(fname_pattern)):
+        print("Opening {}".format(fname))
+        files.append(open(fname, 'r'))
+
+    for i, file in enumerate(files):
+        fname0 = file.name
+        fname1 = fname_pattern.replace('*', str(i))
+        assert fname0 == fname1
+
+    return files
+
+
+files = open_files(fname_pattern)
+
+lines = ["" for i in range(len(files))]
+print("while loop")
+for i, file in enumerate(files):
+    lines[i] = file.readlines()
+
+nlines = len(lines[0])
+
+out_lines = []
+for i in range(nlines):
+    out_lines.append(lines[0][i])
+    cur_floats = None
+    for file_lines in lines:
+        arr_line = file_lines[i].split(" ")
+        filtered = list(filter(lambda t: t != '', arr_line))
+        filtered[-1] = filtered[-1][:-1]
+        try:
+            floats = list(map(float, filtered))
+            if cur_floats is None:
+                cur_floats = floats
+            else:
+                for i in range(len(cur_floats)):
+                    cur_floats[i] = max(cur_floats[i], floats[i])
+        except:
+            pass
+    if cur_floats is not None:
+        out_lines[-1] = " ".join(map(str, cur_floats)) + "\n"
+        cur_floats = None
+
+with open("merged_vv.log", 'w') as outfile:
+    for line in out_lines:
+        outfile.write(line)
