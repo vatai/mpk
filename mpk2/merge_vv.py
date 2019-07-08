fd294af5a9b6effee7b3953165979430c89f9f49
Emil VATAI <emil.vatai@gmail.com>
2019-07-08 18:48:49 +0900

    Rewrite `merge_vv.py`

--- mpk2/merge_vv.py
+++ mpk2/merge_vv.py
@@ -1,49 +1,54 @@
+"""Merge text log files. """
+
 import sys
-import glob
-
-N = 4
-fname_pattern = sys.argv[1]
+import re
 
 
-def open_files(fname_pattern):
+def open_files():
     """Open files. """
     files = []
-    for fname in sorted(glob.glob(fname_pattern)):
+    for fname in sorted(sys.argv[1:]):
         print("Opening {}".format(fname))
         files.append(open(fname, 'r'))
 
     for i, file in enumerate(files):
         fname0 = file.name
-        fname1 = fname_pattern.replace('*', str(i))
+        fname1 = re.sub(r'[0-9]', str(i), sys.argv[1])
         assert fname0 == fname1
 
     return files
 
 
-files = open_files(fname_pattern)
+def all_same(lines):
+    line = lines[0]
+    for other in lines:
+        if other != line:
+            return False
+    return True
 
-lines = ["" for i in range(len(files))]
-print("while loop")
-for i, file in enumerate(files):
-    lines[i] = file.readlines()
 
-nlines = len(lines[0])
-
-out_lines = []
-for i in range(nlines):
-    cur_floats = None
-    for file_lines in lines:
-        arr_line = file_lines[i].split(" ")
-        filtered = list(filter(lambda t: t != '', arr_line))
-        filtered[-1] = filtered[-1][:-1]
-        if '>' in filtered or '' in filtered:
-            print(lines[0][i][:-1])
+def proc_lines(lines):
+    mf = None
+    for line in lines:
+        floats = map(float, filter(lambda t: t != '', line.split(" ")))
+        floats = list(floats)
+        if mf is None:
+            mf = floats
         else:
-            floats = map(float, filtered)
-            if cur_floats is None:
-                cur_floats = list(floats)
-            else:
-                for j in range(len(cur_floats)):
-                    cur_floats[j] = max(cur_floats[j], floats[j])
-            line = " ".join(map(str, cur_floats))
-            print(line)
+            for i, m in enumerate(mf):
+                mf[i] = max(floats[i], m)
+    return ", ".join(map(str, mf))
+
+
+files = open_files()
+
+all_lines = [file.readlines() for file in files]
+
+transposed = zip(*all_lines)
+
+for lines in transposed:
+    if all_same(lines):
+        print(lines[0][:-1])
+    else:
+        mline = proc_lines(lines)
+        print(mline)
