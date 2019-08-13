fc9d5566cadc41c756e3bedd8f45ccf0156afc14
Emil VATAI <emil.vatai@gmail.com>
2019-08-13 11:41:22 +0900

    Prune mpkrun (for val reading)

--- mpk2/mpkrun.c
+++ mpk2/mpkrun.c
@@ -6,45 +6,6 @@
 #include "lib.h"
 
 
-void show_exinfo(mpk_t *mg) {
-  assert(mg != NULL);
-
-#if DETAIL == 0
-  return;
-#endif
-
-  int npart = mg->npart;
-  int nphase = mg->nphase;
-
-  int i;
-  double tsum = 0.0;
-  int maxth = 0;
-  for (i=0; i<= nphase; i++) {
-    int j;
-    for (j=0; j< npart; j++) {
-      task_t *tl = mg->tlist + i * npart + j;
-#if DETAIL
-      printf("%d %d %d %d %e %e\n", i, j, tl->th,
-	     tl->n, tl->t1 - tl->t0, (tl->t1 - tl->t0) / tl->n);
-#endif
-      tsum += tl->t1 - tl->t0;
-      if (maxth < tl->th)
-	maxth = tl->th;
-    }
-  }
-  printf("total work %e, avrg %e\n", tsum, tsum/(maxth+1.0));
-}
-
-void check_error(double *vv, int n, int nlevel) {
-  int i;
-  double e = 0.0;
-  for (i=0; i< n; i++) {
-    double v = vv[n * nlevel + i];
-    e += (v - 1.0) * (v - 1.0);
-  }
-  printf("error %e\n", sqrt(e/n));
-}
-
 // new stuff
 void vexec_mpk(mpk_t *mg, double *vv, double *val);
 
@@ -64,6 +25,7 @@ double *alloc_read_val(mpk_t *mg, char *ptr) {
   sprintf(fname, "%s", ptr);
   sprintf(fname + cnt, "%s", ".val");
   FILE *file = fopen(fname, "r");
+  printf("Val file: %s\n", fname);
   for (int i = 0; i < mg->g0->ptr[mg->n]; i++)
     fscanf(file, "%lf ", val + i);
   fclose(file);
@@ -106,10 +68,7 @@ int main(int argc, char **argv) {
     vv[mg->n + i] = -1.0;		/* dummy */
 
   vexec_mpk(mg, vv, val);
-
-  check_error(vv, mg->n, mg->nlevel);
   write_results(mg, vv, argv[1]);
-  show_exinfo(mg);
 
   return 0;
 }
