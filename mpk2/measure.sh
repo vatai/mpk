ec9f74d3e3db330a3f7b3754b3726f3344b0e816
Emil VATAI <emil.vatai@gmail.com>
2019-08-19 22:30:29 +0900

    Add clean name (remove underscores)

--- mpk2/measure.sh
+++ mpk2/measure.sh
@@ -3,17 +3,17 @@
 function proc_file {
     NAME=$(echo $(basename $1) | sed s/\.tar\.gz//)
     tar xzf $1
-    mv ${NAME}/${NAME}.mtx .
+    MTXNAME=$(echo ${NAME} | sed s/_/-/g).mtx
+    mv ${NAME}/${NAME}.mtx ${MTXNAME}
     for lvl in $(seq 10 10 20); do # 10 20 .. 50
         for phs in 0 1 2 3; do
-            NPART=16 NPHASE=${phs} NLEVEL=${lvl} ./test_mtx.sh ${NAME}.mtx
+            NPART=16 NPHASE=${phs} NLEVEL=${lvl} ./test_mtx.sh ${MTXNAME}
         done
     done
     # rm -rf ${NAME}
 }
 
 function proc_dir {
-    ls $1
     ls $1 | while read file; do
         FULL=$1/$file
         proc_file ${FULL}
