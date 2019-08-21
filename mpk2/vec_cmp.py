928c30d5c5f658cd9e1e55bfef664d5edac4ea7c
Emil VATAI <emil.vatai@gmail.com>
2019-08-21 12:10:38 +0900

    vec_cmp.py takes args

--- mpk2/vec_cmp.py
+++ mpk2/vec_cmp.py
@@ -7,6 +7,7 @@
 # norms of the errors/diffs).
 
 import struct
+import sys
 
 import numpy as np
 
@@ -17,11 +18,19 @@ def read_arr(n, arr, file):
         arr[i] = struct.unpack("d", file.read(8))[0]
 
 
-n, nlevel = 4410, 10
+# vf = open("bcsstk28_2_10_3/results.bin", "rb")
+# gf = open("bcsstk28_2_10_3/gold_result.bin", "rb")
+vname = sys.argv[3]
+gname = sys.argv[4]  # gold
+print("vname {}, gname {}".format(vname, gname))
+vf = open(vname, "rb")
+gf = open(gname, "rb")
+
+# n, nlevel = 4410, 10
+n = int(sys.argv[1])
+nlevel = int(sys.argv[2])
 vect = np.zeros(n)
 gold = np.zeros(n)
-vf = open("bcsstk28_2_10_3/results.bin", "rb")
-gf = open("bcsstk28_2_10_3/gold_result.bin", "rb")
 
 for _ in range(nlevel + 1):
     read_arr(n, vect, vf)
