157886ffd77d164435ee8d6b4f33691550e1ded8
Emil VATAI <emil.vatai@gmail.com>
2019-08-31 21:01:47 +0900

    Remove preprocessing

--- mpk2/mpi_gen.sh
+++ mpk2/mpi_gen.sh
@@ -20,13 +20,4 @@ srun -N 1 ./gen2 $NAME $SIZE $NAME$SIZE
 srun -N 1 ./driver $NAME$SIZE $NPART $NLEVEL $NPHASE 1>/dev/null || exit 1
 cp $NAME$SIZE.loop.g0 $DIRNAME/loop.g0 || exit 2
 
-# POST PROCESSING
-#
-# Assume 2d 5point mesh.  The level and partition of vertices is
-# written 1 vertex per line.  Rearrange the level and partition values
-# in a way that resembles the original 2d mesh.
-for file in $(ls $DIRNAME/l[0-9]* $DIRNAME/g*part*); do
-    perl -lne 'if ($. % '$SIZE' == 0) {print "$p $_"; $p=""} else { $p="$p $_"}' $file > $file.pp
-done
-
 srun -N $NPART ./run_script.sh $DIRNAME
