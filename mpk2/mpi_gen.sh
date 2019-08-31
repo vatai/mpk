b726780f3ef47f368c25a83e61c8d870d84e5205
Emil VATAI <emil.vatai@gmail.com>
2019-08-31 20:47:58 +0900

    Update NAME to be matrix type in mpi_get.sh

--- mpk2/mpi_gen.sh
+++ mpk2/mpi_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p p
-NAME=${NAME:=mesh5p}
+NAME=${NAME:=m5p}
 SIZE=${SIZE:=100}
 NPART=${NPART:=2}
 NLEVEL=${NLEVEL:=10}
@@ -15,8 +15,8 @@ make || exit -1
 # Delete input files.
 rm -rf ${DIRNAME}
 
-srun -N 1 ./gen m5p $SIZE $NAME$SIZE
-srun -N 1 ./gen2 m5p $SIZE $NAME$SIZE
+srun -N 1 ./gen $NAME $SIZE $NAME$SIZE
+srun -N 1 ./gen2 $NAME $SIZE $NAME$SIZE
 srun -N 1 ./driver $NAME$SIZE $NPART $NLEVEL $NPHASE 1>/dev/null || exit 1
 cp $NAME$SIZE.loop.g0 $DIRNAME/loop.g0 || exit 2
 
---
+++ mpk2/sbatch_gen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# SIZEITER="10 50 100 200"
+# NPARTITER="2 4 8 16"
+# NPHASEITER=$(seq 0 5)
+SIZEITER="10 20"
+NPARTITER="2 4"
+NPHASEITER=$(seq 1 2)
+
+# for NAME in m{3,5,7}p t{3,5,7}p cube; do #
+for NAME in m3p t3p cube; do #
+    for SIZE in $SIZEITER; do #
+        for NPART in $NPARTITER; do #
+            for NPHASE in $NPHASEITER; do #
+                NAME=$NAME SIZE=$SIZE NPART=$NPART NPHASE=$NPHASE sbatch -N $NPART mpi.sh
+            done
+        done
+    done
+done
