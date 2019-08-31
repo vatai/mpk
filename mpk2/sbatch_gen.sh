ef3d4f9831d80c6718eee66260b2062655453ab0
Emil VATAI <emil.vatai@gmail.com>
2019-08-31 21:32:02 +0900

    Real execution

--- mpk2/sbatch_gen.sh
+++ mpk2/sbatch_gen.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# SIZEITER="10 50 100 200"
-# NPARTITER="2 4 8 16"
-# NPHASEITER=$(seq 0 5)
-SIZEITER="10 20"
-NPARTITER="2 4"
-NPHASEITER=$(seq 1 2)
+SIZEITER="10 50 100 200"
+NPARTITER="2 4 8 16"
+NPHASEITER=$(seq 0 5)
+# SIZEITER="10 20"
+# NPARTITER="2 4"
+# NPHASEITER=$(seq 1 2)
 
-# for NAME in m{5,7,9}p t{5,7,9}p cube; do #
-for NAME in m5p t5p cube; do #
+for NAME in m{5,7,9}p t{5,7,9}p cube; do #
+# for NAME in m5p t5p cube; do #
     for SIZE in $SIZEITER; do #
         for NPART in $NPARTITER; do #
             for NPHASE in $NPHASEITER; do #
