977d759d5d0136f1f397bc8ef60bff48fb748de8
Emil VATAI <emil.vatai@gmail.com>
2019-08-31 22:27:31 +0900

    Add batch_mtx.sh

---
+++ mpk2/batch_mtx.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+SIZEITER="10 100"
+NPARTITER="4 8"
+NPHASEITER=$(seq 0 4)
+# SIZEITER="10 20"
+# NPARTITER="2 4"
+# NPHASEITER=$(seq 1 2)
+
+for NAME in $(ls *.mtx); do #
+# for NAME in m5p t5p cube; do #
+    for SIZE in $SIZEITER; do #
+        for NPART in $NPARTITER; do #
+            for NPHASE in $NPHASEITER; do #
+                NAME=$NAME SIZE=$SIZE NPART=$NPART NPHASE=$NPHASE ./test_mtx.sh
+            done
+        done
+    done
+done
