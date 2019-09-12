47ef41c2579f792df641fbd1056236a481d4759c
Emil VATAI <emil.vatai@gmail.com>
2019-09-12 11:58:24 +0900

    Start read_pcd()

--- mpk2/partial_cd.h
+++ mpk2/partial_cd.h
@@ -2,11 +2,13 @@
 #define _PARTIAL_H_
 
 struct partial_cd {
-  char *dir;
+  const char *dir;
   int rank;
 };
 
 struct partial_cd *
 new_partial_cd(char *dir, int rank);
+
+void read_pcd(struct partial_cd* pcd);
 
 #endif
