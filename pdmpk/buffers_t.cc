6aa5169d68989abfa52eb7eba69943115651c6b1
Emil VATAI <emil.vatai@gmail.com>
2019-09-19 19:43:06 +0900

    Added notes and binary file writing method

--- pdmpk/buffers_t.cc
+++ pdmpk/buffers_t.cc
@@ -1,1 +1,12 @@
+#include <fstream>
+#include <sstream>
+
 #include "buffers_t.h"
+
+void buffers_t::dump()
+{
+  double xvar = 3.14;
+  std::stringstream fname;
+  fname << "fname";
+  std::ofstream file(fname.str(), std::ios::binary);
+}
--- pdmpk/buffers_t.h
+++ pdmpk/buffers_t.h
@@ -2,12 +2,48 @@
 #define _BUFFERS_T_H_
 
 #include <forward_list>
+#include <utility>
+#include <map>
 
 #include <metis.h>
 
+#include "typedefs.h"
+
+// static void mpk_exec_bufs_val(buffers_t *bufs) {
+//   do_task(bufs, 0);
+//   for (int phase = 1; phase <= bufs->nphase; phase++) {
+//     do_comm(phase, bufs);
+//     do_task(bufs, phase);
+//   }
+// }
+
 class buffers_t {
 public:
-  std::forward_list<idx_t> idx_buf;
+  // // npart * (nphase + 1)
+  // int *recvcounts;
+  // int *sendcounts;
+  // int *rdispls;
+  // int *sdispls;
+
+  // // nphase + 1 // int *rcount; // int *mcount; // int *scount;
+
+  // int *rbuf_offsets;
+  // int *mbuf_offsets;
+  // int *sbuf_offsets;
+  // int *mptr_offsets;
+  // int *mcol_offsets;
+
+  std::forward_list<std::pair<idx_t, level_t>> pair_buf;
+  // long *idx_buf; // Important note in iterator() function comments
+  // long *idx_sbuf;
+  std::forward_list<std::pair<idx_t, level_t>> pair_sbuf;
+  // long *mptr_buf;
+  // long *mcol_buf;
+
+  // double *vv_buf;
+  // double *vv_sbuf;
+  // double *mval_buf;
+  void dump();
 };
 
 #endif
