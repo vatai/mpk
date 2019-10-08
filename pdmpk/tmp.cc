c9e81cc4fc6aad7547ddc2cdf9ea8286952a0a7a
Emil VATAI <emil.vatai@gmail.com>
2019-10-08 23:36:52 +0900

    Add vector.insert() check

--- pdmpk/tmp.cc
+++ pdmpk/tmp.cc
@@ -3,6 +3,17 @@
 
 int main(int argc, char *argv[])
 {
+  std::vector<int> a = {1, 2, 3};
+  std::vector<int> b = {10, 20, 30};
+  std::cout << "a: ";
+  for (auto e : a) std::cout << e << ", ";
+  std::cout << "\n";
+
+  b.insert(std::begin(b) + 1, std::begin(a), std::end(a));
+  std::cout << "b: ";
+  for (auto e : b) std::cout << e << ", ";
+  std::cout << "\n";
+
   comm_dict_t cdict(3);
   cdict.record(0, 2, 10);
   cdict.record(0, 1, 20);
