1c1feaba1ec413eb3a8c6c15f8011bcd6a7a4c31
Emil VATAI <emil.vatai@gmail.com>
2019-10-10 22:55:23 +0900

    Add add_to_{mptr,mcol} to buffers_t_new

--- pdmpk/buffers_t_new.cc
+++ pdmpk/buffers_t_new.cc
@@ -2,8 +2,40 @@
 
 #include "buffers_t_new.h"
 
-buffers_t::buffers_t(const idx_t _npart)
-    : npart {_npart}
+buffers_t::buffers_t(const idx_t npart)
+    : npart {npart},
+      mcsr_bufs(npart)
 {
   std::cout << "basement" << std::endl;
+}
+
+void buffers_t::add_to_mptr(const size_t rank, const idx_t val)
+{
+  mcsr_bufs[rank].mptr_count[phase]++;
+  mcsr_bufs[rank].mptr.push_back(val);
+}
+
+void buffers_t::add_to_mcol(const size_t rank, const idx_t val)
+{
+  mcsr_bufs[rank].mcol_count[phase]++;
+  mcsr_bufs[rank].mcol.push_back(val);
+}
+
+void buffers_t::pre_phase()
+{
+  std::cout << "pre_phase()" << std::endl;
+  for (int r = 0; r < npart; r++) {
+    auto &buf = mcsr_bufs[r];
+    buf.mptr_count.push_back(0);
+    buf.mcol_count.push_back(0);
+  }
+}
+
+void buffers_t::post_phase(comm_dict_t &cd)
+{
+  std::cout << "post_phase()" << std::endl;
+  cd.process();
+  // insert {recv,send}{count,displs}
+  // implant rbuf, sbuf, ibuf
+  cd.clear();
 }
--- pdmpk/buffers_t_new.h
+++ pdmpk/buffers_t_new.h
@@ -4,23 +4,36 @@
 
 #include <metis.h>
 
+#include "comm_dict_t.h"
 #include "csr_t.h"
 
-class mpi_comm_data {
-  std::vector<int> recvcounts;
-  std::vector<int> sendcounts;
-  std::vector<int> rdispls;
-  std::vector<int> sdispls;
+struct mcsr {
+  std::vector<idx_t> mptr;
+  std::vector<size_t> mptr_count;
+  std::vector<size_t> mptr_offset;
+
+  std::vector<idx_t> mcol;
+  std::vector<size_t> mcol_count;
+  std::vector<size_t> mcol_offset;
 };
 
 class buffer_t {
   std::vector<csr_t> buffers;
   std::vector<std::vector<double>> mval;
-
-
 };
 
 class buffers_t {
-  const idx_t npart;
+ public:
   buffers_t(const idx_t _npart);
+  void pre_phase();
+  void post_phase(comm_dict_t &cd);
+  void add_to_mptr(const size_t rank, const idx_t val);
+  void add_to_mcol(const size_t rank, const idx_t val);
+
+  int phase;
+
+  // @todo(vatai): record_phase() ??? sbuf_offset.push_back(sbuf.size()) etc
+ private:
+  const idx_t npart;
+  std::vector<mcsr> mcsr_bufs;
 };
