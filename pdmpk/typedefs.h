844b839acc7b00a74e64ad0fbc53255eff32750c
Emil VATAI <emil.vatai@gmail.com>
2019-10-12 11:21:09 +0900

    Add from_to_pair_t to typedefs.h

--- pdmpk/typedefs.h
+++ pdmpk/typedefs.h
@@ -1,7 +1,12 @@
 #ifndef _TYPEDEFS_H_
 #define _TYPEDEFS_H_
 
+#include <utility>
+
+#include <metis.h>
+
 typedef unsigned short level_t;
+typedef std::pair<idx_t, idx_t> from_to_pair_t;
 
 #endif
 
