e57025be00dc81e77bdcca05e550cf6e6ca66cb3
Emil VATAI <emil.vatai@gmail.com>
2019-10-14 08:00:02 +0900

    Add buffer description to mainpage

--- pdmpk/pdmpk_prep.cc
+++ pdmpk/pdmpk_prep.cc
@@ -2,14 +2,46 @@
  * @author Emil VATAI <emil.vatai@gmail.com>
  * @date 2019-09-17
  *
- * @mainpage Partial Diamon Matrix Powers Kernel
+ * @mainpage Partial Diamon Matrix Powers Kernel (PDMPK)
  *
  * Given a (sparse) matrix \f$A \in \mathbb{R}^{n \times n}\f$ and
- *   vector \f$v \in \mathbb{R}\f$ MPK generates \f[ Av, A^2v, \ldots,
- *   A^m v \f]
+ * vector \f$v \in \mathbb{R}\f$ MPK generates \f[ Av, A^2v, \ldots,
+ * A^m v \f]
  *
- * This program generates the communication pattern for a given matrix
- * \f$A\f$.
+ * This program generates the "computation and communication pattern"
+ * for a given matrix \f$A\f$. The computation and communication
+ * pattern mentioned above is a set of "buffers", or arrays, which can
+ * be contiguously stored in memory, or on disk, and processed, that
+ * is used to make the above vectors of the MPK.  The processing takes
+ * place in multiple phases of local computation, separated by
+ * communication.  The buffers describing the communication and
+ * computation pattern is described here:
+ *
+ * - `mptr`, `mcol`, `mval`: the calculation itself is
+ *   very similar to a regular SpMV operation with CSR format and uses
+ *   these arrays
+ *   @see mpi_bufs_t
+ *   @see partial_cd
+ *
+ * - `ibuf`, `sbuf` and `ibuf`:
+ *
+ * - `sendcount`, `recvcount`, `sdispls` and `rdispls`: are the
+ *   buffers corresponding to the `MPI_Alltoallv()` parameters.  These
+ *   buffers have a fixed size of `nphase * npart * npart`.
+ *
+ * Since segments of most of these buffers are accessed per phase, if
+ * a buffer `buf` (here `buf` is the name of any of the above buffers)
+ * has a corresponding `buf_count` and/or `buf_offset` array, then for
+ * phase `t` the relevant segment of `buf` is:
+ *
+ * - `buf_count` only case:
+ *   - if `t == 0`, then `buf[0..buf_count[t]]`,
+ *   - if `t > 0`, then `buf[buf_count[t - 1]..buf_count[t]]`
+ *
+ * - `buf_offset` only case: `buf[buf_offset[t]..buf_offset[t + 1]]`
+ *
+ * - `buf_offset` and `buf_count` case:
+ *   `buf[buf_offset[t]..buf_offset[t] + buf_count[t]]`
  *
  * @todo(vatai) Finish documentation here.
  * @todo(vatai): Remove rank if not needed.
