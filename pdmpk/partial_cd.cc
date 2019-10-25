98d57140d0779f79dbd790a8b6ad86ea91246064
Emil VATAI <emil.vatai@gmail.com>
2019-10-25 14:08:25 +0900

    Fix `src_send_base()` and `tgt_recv_base()`
    Both of these functions had the same error of reporting their values
    always for `phase == 0` only.

--- pdmpk/partial_cd.cc
+++ pdmpk/partial_cd.cc
@@ -245,10 +245,10 @@ void partial_cd::set_store_part(const idx_t idx, const level_t level, const idx_
 
 idx_t partial_cd::src_send_base(const sidx_tidx_t src_tgt) const
 {
-  return bufs[src_tgt.first].mpi_bufs.sdispls[src_tgt.second];
+  return bufs[src_tgt.first].mpi_bufs.sdispls[phase * npart + src_tgt.second];
 }
 
 idx_t partial_cd::tgt_recv_base(const sidx_tidx_t src_tgt) const
 {
-  return bufs[src_tgt.second].mpi_bufs.rdispls[src_tgt.first];
+  return bufs[src_tgt.second].mpi_bufs.rdispls[phase * npart + src_tgt.first];
 }
