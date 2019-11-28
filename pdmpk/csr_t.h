bb166ed6f4c4758e3103dac50e9039129a0b72da
Emil VATAI <emil.vatai@gmail.com>
2019-11-28 18:21:29 +0900

    Add `spmv` and `mpk` declarations to `csr_t`

--- pdmpk/csr_t.h
+++ pdmpk/csr_t.h
@@ -14,6 +14,8 @@
 class csr_t {
  public:
   csr_t(const char* fname);
+  std::vector<double> spmv(const std::vector<double>& vec) const;
+  void mpk(const int nlevels, std::vector<double>& vec) const;
   idx_t n;
   idx_t nnz;
   std::vector<idx_t> ptr;
