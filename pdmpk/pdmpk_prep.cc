ee6f3c98e577cafa746de031f1f06cb87a822aad
Emil VATAI <emil.vatai@gmail.com>
2019-11-28 23:25:43 +0900

    Add todo for communication optimisation

--- pdmpk/pdmpk_prep.cc
+++ pdmpk/pdmpk_prep.cc
@@ -53,6 +53,8 @@
  * @todo(vatai): Change comm_dict to store sets instead of vectors to
  * avoid duplicates in sbuf.
  *
+ * @todo(vatai): Implement communication optimisation.
+ *
  * @todo(vatai): Implement custom input vector loading.
  *
  * @todo(vatai): Collect results.
