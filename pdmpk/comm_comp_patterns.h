fdead21988304e2836cf67433d1005b2e2fd41df
Emil VATAI <emil.vatai@gmail.com>
2019-12-19 15:50:31 +0900

    Clean up documentation around `CommDict`

--- pdmpk/comm_comp_patterns.h
+++ pdmpk/comm_comp_patterns.h
@@ -34,31 +34,45 @@ private:
   /// All MPK buffers such as levels, weights, partitions, and
   /// partials.
   PDMPKBuffers pdmpk_bufs;
+
   /// "Shadow" copy of `pdmpk_bufs` used for optimization.
   PDMPKBuffers pdmpk_count;
 
   /// Map (vector index, level) pair to the (partition, mbuf index)
   /// pair where it can be found.
   typedef std::map<idx_lvl_t, part_sidx_t> StorePart;
+
+  /// @see StorePart
   StorePart store_part;
 
-  /// In each phase, collect the communication of complete indices as
-  /// a map from (source, target) pairs to `mbuf` indices of the
-  /// source partition. (In case of type = kMcol)
-  /// Also, collect the communication of partial indices (for
-  /// initialization) as a map from (source, target) pairs to (mbuf
-  /// indices of source partition, mcol indices in the target
-  /// partition) pairs. (In case of type = kInitIdcs)
-  enum CommType { kMcol, kInitIdcs };
+  /// The type of communication recorded in @ref CommDict @see
+  /// comm_dict
+  enum CommType {
+    kMcol,    ///< Full vertex (needs update `mcol` in @ref MCSR)
+    kInitIdcs ///< Partial/initialization value (needs update
+              ///`init_idcs` in @ref Buffers)
+  };
+
+  /// A structure describing the data needed to be communicated.
   struct SrcType {
-    idx_t src_mbuf_idx;
-    CommType type;
+    idx_t src_mbuf_idx; ///< The index in `mbuf` (in @ref Buffers) where the
+                        /// data can be found, obtained from @ref store_part
+    CommType type;      ///< The type of communication @see CommType
     friend bool operator<(const SrcType &l, const SrcType &r) {
       return std::tie(l.src_mbuf_idx, l.type) <
              std::tie(r.src_mbuf_idx, r.type);
     }
   };
-  typedef std::map<src_tgt_t, std::map<SrcType, std::set<idx_t>>> CommDict;
+
+  /// For each (mbuf index, type) pair, lists all the indices needed
+  /// to be updated (the mbuf index is in the source partition, the
+  /// updated indices are on the target partition). @see ProcCommDict
+  typedef std::map<SrcType, std::set<idx_t>> Backpatch;
+
+  /// In each phase, collect the communication as a map from (source,
+  /// target) pairs to a @ref Backpatch.
+  typedef std::map<src_tgt_t, Backpatch> CommDict;
+  /// @see CommDict
   CommDict comm_dict;
 
   typedef std::map<src_tgt_t, std::set<idx_t>> CommTable;
@@ -78,6 +92,15 @@ private:
 
   void FinalizePhase();
   void UpdateMPICountBuffers(const src_tgt_t &src_tgt_part, const size_t size);
+
+  /// Process every @ref CommDict. The key of a CommDict determines
+  /// the source and target partitions, the @ref Backpatch determines
+  /// what needs to be done. The @ref SrcType::src_mbuf_idx in the
+  /// index added to @ref Buffers::sbuf, the @ref SrcType::type
+  /// determines which what needs to be updated (@ref MCSR::mcol for
+  /// @ref kMcol, @ref MPIBuffers::init_idcs for @ref kInitIdcs), and
+  /// the set mapped to by the backpatch contains all the indices
+  /// which need to be updated.
   void ProcCommDict(const CommDict::const_iterator &iter);
 
   /// Return the base (0th index) of the subinterval of send buffer in
