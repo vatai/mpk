db47bd3679c6f745c46b5a4ed8a624ad5323d063
Emil VATAI <emil.vatai@gmail.com>
2019-12-20 22:35:04 +0900

    Add batch_proc script

---
+++ scripts/batch_proc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Author: Emil VATAI <emil.vatai@gmail.com>
+# Date: 2019-12-20
+
+# Script to execute pdmpk on matrices.
+
+# Optional input variables
+NLEVEL_ITER=${NLEVEL_ITER:="10"}
+NPART_ITER=${NPART_ITER:="4"}
+PREFIX=${PREFIX:=.}
+MPIRUN=${MPIRUN:=orterun}
+
+function single_run() {
+    local MATRIX=$1
+    local NPART=$2
+    local NLEVEL=$3
+    local PREP=$PREFIX/pdmpk_prep
+    local EXEC=$PREFIX/pdmpk_exec
+    local TEST=$PREFIX/pdmpk_test
+    echo Processing $MATRIX
+    $PREP $MATRIX $NPART $NLEVEL
+    $MPIRUN -n $NPART $EXEC $MATRIX
+    # $TEST $MATRIX $NPART $NLEVEL
+}
+
+function proc_matrix() {
+    local MATRIX=$1
+    for NLEVEL in $NLEVEL_ITER; do
+        for NPART in $NPART_ITER; do
+            single_run $MATRIX $NPART $NLEVEL
+        done
+    done
+}
+
+for MATRIX in "$@"; do
+    proc_matrix $MATRIX
+done
+
