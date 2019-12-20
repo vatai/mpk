1cd791240fb6a89110f478ebc2598925006d6a69
Emil VATAI <emil.vatai@gmail.com>
2019-12-20 12:08:04 +0900

    Add useful constructs

--- scripts/gen_matrices.sh
+++ scripts/gen_matrices.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-echo 
-read -n 1 -p "Press any key to continue"
-echo $0
-pwd
-dirname $(pwd)
-basename $(pwd)
+
+TYPE_ITER="m5p m9p"
+SIZE_ITER="10 20"
+
+function list_matrices() {
+    echo "The following matrices will be generated in the current directory."
+    for TYPE in $TYPE_ITER; do
+        for SIZE in $SIZE_ITER; do
+            NAME=${TYPE}${SIZE}
+            echo $NAME
+        done
+    done
+    read -n 1 -p "Press any key to continue"
+}
+
+function gen_matrices() {
+}
+
+list_matrices
+gen_matrices
+
+# NAME=${NAME:=m5p}
+# SIZE=${SIZE:=100}
+# echo $0
+# pwd
+# dirname $(pwd)
+# basename $(pwd)
