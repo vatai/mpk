0755b38ca2707a4667d53006b88874b40327e951
Emil VATAI <emil.vatai@gmail.com>
2019-12-20 12:50:44 +0900

    Generating script finished

--- scripts/gen_matrices.sh
+++ scripts/gen_matrices.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
+# Author: Emil VATAI <emil.vatai@gmail.com>
+# Date: 2019-12-20
 
-TYPE_ITER="m5p m9p"
-SIZE_ITER="10 20"
+# Script to generate matrices.
+
+# Optional input variables
+TYPE_ITER=${TYPE_ITER:="m5p m9p"}
+SIZE_ITER=${SIZE_ITER:="10 20"}
 
 function list_matrices() {
     echo "The following matrices will be generated in the current directory."
     for TYPE in $TYPE_ITER; do
         for SIZE in $SIZE_ITER; do
-            NAME=${TYPE}${SIZE}
+            local NAME=${TYPE}${SIZE}
             echo $NAME
         done
     done
     read -n 1 -p "Press any key to continue"
+    echo
+}
+
+function gen_matrix() {
+    local GEN2=$(dirname $0)/../mpk2/gen2
+    local GEN2MTX=$(dirname $0)/../mpk2/gen2mtx
+    local TYPE=$1
+    local SIZE=$2
+    local NAME=$1$2
+    $GEN2 $TYPE $SIZE $NAME
+    $GEN2MTX $NAME.loop.g0
+    rm $NAME.loop.{co,val,g0}
 }
 
 function gen_matrices() {
+    for TYPE in $TYPE_ITER; do
+        for SIZE in $SIZE_ITER; do
+            gen_matrix $TYPE $SIZE
+        done
+    done
 }
 
 list_matrices
 gen_matrices
 
-# NAME=${NAME:=m5p}
-# SIZE=${SIZE:=100}
-# echo $0
-# pwd
-# dirname $(pwd)
-# basename $(pwd)
