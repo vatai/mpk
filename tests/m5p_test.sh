c3e34e2b7b9428cf6d3dba6b15c5a9eaae1331e2
Emil VATAI <emil.vatai@gmail.com>
2019-12-21 19:22:43 +0900

    Make parameterised test for generated matrices

---
+++ tests/m5p_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Author: Emil VATAI <emil.vatai@gmail.com>
+# Date: 2019-12-21
+
+### Run a single test for generated matrices. The parameters are SIZE
+### of the mesh, NPART number of processes/partitions and NLEVEL
+### number of levels calculated by MPK.
+
+SCRIPTS_DIR=${SCRIPTS_DIR:=$(dirname $0)/../scripts}
+MATRICES_DIR=${MATRICES_DIR:=$(dirname $0)/../matrices}
+
+TYPE=${1:-m5p} # $1 or defaults to m5p.
+SIZE=${2:-4}   # $2 or defaults to 4.
+NPART=${3:-4}  # $3 or defaults to 4.
+NLEVEL=${4:-4} # $4 or defaults to 4.
+
+# Create input matrix if it doesn't exist.
+mkdir -p "$MATRICES_DIR"
+MATRIX="$MATRICES_DIR/m5p${SIZE}.loop.mtx"
+if [ ! -f "$MATRIX" ]; then
+    SIZE_ITER=${SIZE} TYPE_ITER=m5p ${SCRIPTS_DIR}/gen_matrices.sh ${MATRICES_DIR}
+fi
+
+# Run test.
+NLEVEL_ITER=$NLEVEL NPART_ITER=$NPART $SCRIPTS_DIR/batch_proc.sh $MATRIX
