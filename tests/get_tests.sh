d6e6bc1fa731a5707875b1309752d4c2c8a15919
Emil VATAI <emil.vatai@gmail.com>
2019-12-21 23:17:56 +0900

    Add get_tests.sh

---
+++ tests/get_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Author: Emil VATAI <emil.vatai@gmail.com>
+# Date: 2019-12-21
+
+### Run a single test for downloaded matrices. The parameters are the
+### URL of the matrix, NPART number of processes/partitions and NLEVEL
+### number of levels calculated by MPK.
+
+SCRIPTS_DIR=${SCRIPTS_DIR:=$(dirname $0)/../scripts}
+MATRICES_DIR=${MATRICES_DIR:=$(dirname $0)/../matrices}
+
+URL=$1 # $1 or defaults to m5p.
+NPART=${2:-4}  # $2 or defaults to 4.
+NLEVEL=${3:-4} # $3 or defaults to 4.
+
+# Download input matrix if it doesn't exist.
+if [ ! -f "$MATRIX" ]; then
+    ${SCRIPTS_DIR}/get_matrices.sh ${URL} ${MATRICES_DIR}
+fi
+
+# Run test.
+MATRIX=$(basename $URL | sed 's/\.tar\.gz$//')
+NLEVEL_ITER=$NLEVEL NPART_ITER=$NPART \
+           $SCRIPTS_DIR/batch_proc.sh $MATRICES_DIR/$MATRIX/$MATRIX.mtx
