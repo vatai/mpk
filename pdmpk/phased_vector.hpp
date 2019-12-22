f5236c6888c6610ea63de55c4bd6d85f2c4f784c
Emil VATAI <emil.vatai@gmail.com>
2019-12-22 14:34:35 +0900

    Make phased_vector args const refs

--- pdmpk/phased_vector.hpp
+++ pdmpk/phased_vector.hpp
@@ -6,8 +6,8 @@
 /// of the first element of `array` which belongs to the given
 /// `phase`.
 template <typename T> class phased_vector : public std::vector<T> {
- public:
+public:
   std::vector<size_t> phase_begin;
   void rec_phase_begin() { phase_begin.push_back(this->size()); };
-  T *get_ptr(const int phase) { return this->data() + phase_begin[phase]; }
+  T *get_ptr(const int &phase) { return this->data() + phase_begin[phase]; }
 };
