fdc8f13d6a71ea8d79dbe7186d0f4101c4acc798
Emil VATAI <emil.vatai@gmail.com>
2020-02-12 14:56:59 +0900

    Remove level sum check

--- pdmpk/comm_comp_patterns.cc
+++ pdmpk/comm_comp_patterns.cc
@@ -72,16 +72,12 @@ void CommCompPatterns::Stats() const {
 
 void CommCompPatterns::ProcAllPhases() {
   bool is_finished = pdmpk_bufs.IsFinished();
-  size_t old_level_sum = 0;
   while (not is_finished and not partition_history.empty()) {
     phase++;
     const auto min_level = pdmpk_bufs.MinLevel();
     const auto level_sum = pdmpk_bufs.ExactLevelSum();
     std::cout << "Phase: " << phase << ", "
               << "ExactLevelSum(): " << level_sum << std::endl;
-    if (level_sum == old_level_sum)
-      break;
-    old_level_sum = level_sum;
     if (min_level < args.nlevel / 2) {
       std::cout << "First branch" << std::endl;
       pdmpk_bufs.MetisPartitionWithWeights();
