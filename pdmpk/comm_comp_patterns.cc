df3246015d462cede5b14c33cac406c1bd38da07
Emil VATAI <emil.vatai@gmail.com>
2020-02-20 21:13:34 +0900

    Select `ProcAllPhasesMinAboveZero()`

--- pdmpk/comm_comp_patterns.cc
+++ pdmpk/comm_comp_patterns.cc
@@ -38,8 +38,8 @@ CommCompPatterns::CommCompPatterns(const Args &args)
   phase = 0;
   ProcPhase(phase);
   // ProcAllPhasesNoMirror();
-  ProcAllPhasesMinAboveHalf();
-  // ProcAllPhasesMinAboveZero();
+  // ProcAllPhasesMinAboveHalf();
+  ProcAllPhasesMinAboveZero();
   // ProcAllPhasesCyclePartitions();
 
   Epilogue();
