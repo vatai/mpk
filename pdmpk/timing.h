59e8be7281cae6c7e5354483b8972dcb832eb530
Emil VATAI <emil.vatai@gmail.com>
2020-03-06 22:27:46 +0900

    Add `timing.h`

---
+++ pdmpk/timing.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <mpi.h>
+
+#define DECLARE_TIMING(s)                                                      \
+  double timeStart_##s;                                                        \
+  double timeDiff_##s;                                                         \
+  double timeTally_##s = 0;                                                    \
+  int countTally_##s = 0
+
+#define START_TIMING(s) timeStart_##s = MPI_Wtime()
+
+#define STOP_TIMING(s)                                                         \
+  timeDiff_##s = (double)(MPI_Wtime() - timeStart_##s);                        \
+  timeTally_##s += timeDiff_##s;                                               \
+  countTally_##s++
+
+#define GET_TIMING(s) (double)(timeDiff_##s / MPI_Wtime())
+
+#define GET_AVERAGE_TIMING(s)                                                  \
+  (double)(countTally_##s ? (double)(timeTally_##s / countTally_##s) : 0)
+
+#define CLEAR_AVERAGE_TIMING(s)                                                \
+  timeTally_##s = 0;                                                           \
+  countTally_##s = 0
