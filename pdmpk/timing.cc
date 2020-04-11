794535c8b9cd428ec6b44c7016dc7a1df19c5528
Emil VATAI <emil.vatai@gmail.com>
2020-04-11 22:45:05 +0900

    Add skeleton code to `timing.h`

--- pdmpk/timing.cc
+++ pdmpk/timing.cc
@@ -7,7 +7,9 @@
 
 Timing::Timing(const size_t &size)
     : comp_start_time(size, 0.0), comp_end_time(size, 0.0),
-      comm_start_time(size, 0.0), comm_end_time(size, 0.0) {}
+      comm_start_time(size, 0.0), comm_end_time(size, 0.0),
+      comp_start_recv(size, 0.0), comp_end_recv(size, 0.0),
+      comm_start_recv(size, 0.0), comm_end_recv(size, 0.0) {}
 
 void Timing::StartDoComp(const size_t &phase) {
   comp_start_time[phase] = MPI_Wtime();
@@ -30,14 +32,34 @@ void Timing::CollectData() {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   if (rank > 0) {
-    // MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int
-    // tag, MPI_Comm comm)
-    std::cout << "Send times from " << rank << std::endl;
+    SendVector(comp_start_time, 0);
+    SendVector(comp_end_time, 0);
+    SendVector(comm_start_time, 0);
+    SendVector(comm_end_time, 0);
   } else {
-    for (int k = 1; k < world_size; k++) {
-      // MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int
-      // tag, MPI_Comm comm, MPI_Status *status)
-      std::cout << "Recv times from " << k << std::endl;
+    for (int r = 1; r < world_size; r++) {
+      RecvVector(&comp_start_recv, r);
+      RecvVector(&comp_end_recv, r);
+      RecvVector(&comm_start_recv, r);
+      RecvVector(&comm_end_recv, r);
+      UpdateSummary();
+      std::cout << "Recv times from " << r << std::endl;
     }
   }
 }
+
+/// PRIVATE
+
+void Timing::SendVector(const std::vector<double> &vec, const int rank) const {
+  MPI_Send(vec.data(), vec.size(), MPI_DOUBLE, rank, 0, MPI_COMM_WORLD);
+}
+
+void Timing::RecvVector(std::vector<double> *vec, const int rank) {
+  MPI_Status status;
+  MPI_Recv(vec->data(), vec->size(), MPI_DOUBLE, rank, 0, MPI_COMM_WORLD,
+           &status);
+}
+
+void Timing::InitSummary() {}
+
+void Timing::UpdateSummary() {}
--- pdmpk/timing.h
+++ pdmpk/timing.h
@@ -30,15 +30,59 @@ private:
   /// Constructor initialising all timing data.
   Timing();
 
+  /// Call MPI send on a given vector.
+  ///
+  /// @param vec Send data from here.
+  ///
+  /// @param rank Target MPI rank.
+  void SendVector(const std::vector<double> &vec, const int rank) const;
+
+  /// Call MPI receive on a given vector.
+  ///
+  /// @param vec Copy received data here.
+  ///
+  /// @param rank Source MPI rank.
+  void RecvVector(std::vector<double> *vec, const int rank);
+
+  /// Initialise summary data from measurements of the current
+  /// (usually `rank == 0`) process.
+  void InitSummary();
+
+  /// Update summary data after receiving the a new set of
+  /// measurements from another MPI process.
+  void UpdateSummary();
+
   /// Starting time of @ref Buffers::DoComp call for each phase.
   std::vector<double> comp_start_time;
 
   /// End time of @ref Buffers::DoComp call for each phase.
   std::vector<double> comp_end_time;
 
-  /// Starting time of @ref Buffers::DoComm call for each phase.
+  /// Starting time of @ref Buffers::DoComm call for each phase.  0th
+  /// element is the last (sendhome).
   std::vector<double> comm_start_time;
 
-  /// End time of @ref Buffers::DoComm call for each phase.
+  /// End time of @ref Buffers::DoComm call for each phase.  0th
+  /// element is the last (sendhome).
   std::vector<double> comm_end_time;
+
+  /// Receive buffers for @ref Timing::comp_start_time from another
+  /// MPI process.
+  std::vector<double> comp_start_recv;
+
+  /// Receive buffers for @ref Timing::comp_end_time from another
+  /// MPI process.
+  std::vector<double> comp_end_recv;
+
+  /// Receive buffers for @ref Timing::comm_start_time from another
+  /// MPI process.
+  std::vector<double> comm_start_recv;
+
+  /// Receive buffers for @ref Timing::comm_end_time from another
+  /// MPI process.
+  std::vector<double> comm_end_recv;
+
+  /// Store the maximum of the per phase durations.  0th is the total
+  /// time maximal duration.
+  std::vector<double> max_time;
 };
