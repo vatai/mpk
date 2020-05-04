8c31ec4508870975490fd10ece26537efee2ff1d
Emil VATAI <emil.vatai@gmail.com>
2020-05-04 23:24:50 +0900

    Switch to `AsyncExec()`

--- apps/exec.cc
+++ apps/exec.cc
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
   buf.Load(rank);
   buf.DbgCheck();
   buf.LoadInput();
-  buf.Exec();
+  buf.AsyncExec();
 
   buf.results.FillVal(buf.mbuf);
   buf.results.Dump(rank);
