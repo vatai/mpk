f7a2d7b6ef23a92d0ff4b3f05b804c24ee8244c0
Emil VATAI <emil.vatai@gmail.com>
2020-05-13 22:19:42 +0900

    Collect data

--- pdmpk/timing.cc
+++ pdmpk/timing.cc
@@ -23,11 +23,26 @@ void Timing::CollectData() {
   int rank, world_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  double tmp;
   if (rank > 0) {
+    global_sum /= count;
+    MPI_Send(&global_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    comp_sum /= count;
+    MPI_Send(&comp_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    comm_sum /= count;
+    MPI_Send(&comm_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
   } else {
-    // Send times to process 0.
     for (int r = 1; r < world_size; r++) {
-      // Recv times from process `r`.
+      MPI_Status status;
+      MPI_Recv(&tmp, 1, MPI_DOUBLE, r, 0, MPI_COMM_WORLD, &status);
+      global_sum += tmp;
+      MPI_Recv(&tmp, 1, MPI_DOUBLE, r, 0, MPI_COMM_WORLD, &status);
+      comp_sum += tmp;
+      MPI_Recv(&tmp, 1, MPI_DOUBLE, r, 0, MPI_COMM_WORLD, &status);
+      comm_sum += tmp;
     }
+    global_sum /= world_size;
+    comp_sum /= world_size;
+    comm_sum /= world_size;
   }
 }
